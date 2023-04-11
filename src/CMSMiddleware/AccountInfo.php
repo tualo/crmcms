cf4f353d33912d8ad75a6c37b1476f31b7100f72
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-04-11 09:48:47 +0200

    check is not null

--- src/CMSMiddleware/AccountInfo.php
+++ src/CMSMiddleware/AccountInfo.php
@@ -11,40 +11,42 @@ class AccountInfo {
         @session_start();
         $db = self::db();
         $crm = CRM::getInstance();
-        if (
-            $crm->get('account')->isLoggedIn() &&
-            is_null($crm->get('account')->get('kundennummer')) &&
-            ($crm->get('account')->get('login_type')=='customer') &&
-            ($data = $db->singleRow('select * from adressen_logins where login = {login}',['login'=>$crm->get('account')->get('login')]))
-        ) {
-            ;
-            $crm->get('account')->set('kundennummer',$data['kundennummer']);
-            $crm->get('account')->set('kostenstelle',$data['kostenstelle']);
-            $crm->get('account')->set('email',$data['email']);
-            
-        }
+        if (!is_null($crm->get('account'))){
+            if (
+                $crm->get('account')->isLoggedIn() &&
+                is_null($crm->get('account')->get('kundennummer')) &&
+                ($crm->get('account')->get('login_type')=='customer') &&
+                ($data = $db->singleRow('select * from adressen_logins where login = {login}',['login'=>$crm->get('account')->get('login')]))
+            ) {
+                ;
+                $crm->get('account')->set('kundennummer',$data['kundennummer']);
+                $crm->get('account')->set('kostenstelle',$data['kostenstelle']);
+                $crm->get('account')->set('email',$data['email']);
+                
+            }
 
-        if (
-            $crm->get('account')->isLoggedIn() &&
-            is_null($crm->get('account')->get('kundennummer')) &&
-            ($crm->get('account')->get('login_type')=='translator') &&
-            ($data = $db->singleRow('select * from uebersetzer_logins where login = {login}',['login'=>$crm->get('account')->get('login')]))
-        ) {
-            $crm->get('account')->set('kundennummer',$data['kundennummer']);
-            $crm->get('account')->set('kostenstelle',$data['kostenstelle']);
-            $crm->get('account')->set('email',$data['email']);
-            
-        }
+            if (
+                $crm->get('account')->isLoggedIn() &&
+                is_null($crm->get('account')->get('kundennummer')) &&
+                ($crm->get('account')->get('login_type')=='translator') &&
+                ($data = $db->singleRow('select * from uebersetzer_logins where login = {login}',['login'=>$crm->get('account')->get('login')]))
+            ) {
+                $crm->get('account')->set('kundennummer',$data['kundennummer']);
+                $crm->get('account')->set('kostenstelle',$data['kostenstelle']);
+                $crm->get('account')->set('email',$data['email']);
+                
+            }
 
-        if (
-            $crm->get('account')->isLoggedIn() &&
-            is_null($crm->get('account')->get('angestelltennummer')) &&
-            ($crm->get('account')->get('login_type')=='employee') &&
-            ($data = $db->singleRow('select * from angestellten_logins where login = {login}',['login'=>$crm->get('account')->get('login')]))
-        ) {
-            $crm->get('account')->set('angestelltennummer',$data['angestelltennummer']);
-            $crm->get('account')->set('email',$data['email']);
-            
+            if (
+                $crm->get('account')->isLoggedIn() &&
+                is_null($crm->get('account')->get('angestelltennummer')) &&
+                ($crm->get('account')->get('login_type')=='employee') &&
+                ($data = $db->singleRow('select * from angestellten_logins where login = {login}',['login'=>$crm->get('account')->get('login')]))
+            ) {
+                $crm->get('account')->set('angestelltennummer',$data['angestelltennummer']);
+                $crm->get('account')->set('email',$data['email']);
+                
+            }
         }
 
     }
