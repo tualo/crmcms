fb85d63bb1396cdb22e4842b0caa90ebb8a4a351
Steffen Borchmann <steffen.borchmann@tualo.de>
2023-05-08 11:17:01 +0200

    test sql

--- src/CMSMiddleware/Translator.php
+++ src/CMSMiddleware/Translator.php
@@ -126,7 +126,13 @@ class Translator {
                     $hash=['did'  => $_REQUEST['did']];
                     $sql='select * from  uebersetzer_sprachen where md5(concat(`kundennummer`,`kostenstelle`,`destination_language`,`destination_language`)) = {did}';
                     $langARR=$db->direct($sql,$hash);
-                    print_r($langARR);
+                    if (
+                        $langARR[0]['kundennummer']==$crm->get('account')->get('kundennummer') && 
+                        $langARR[0]['kostenstelle']==$crm->get('account')->get('kostenstelle')  
+                    ){
+                        $sql='delete from  uebersetzer_sprachen where md5(concat(`kundennummer`,`kostenstelle`,`destination_language`,`destination_language`)) = {did}';
+                        $db->direct($sql,$hash);
+                    }
                 }
 /*                
                 if (
